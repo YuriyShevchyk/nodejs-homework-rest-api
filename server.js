489ac5aa240ded0d5a113ddaf445dbbaea9a2984
YuriyShevchyk <97519385+YuriyShevchyk@users.noreply.github.com>
2023-03-16 18:11:07 +0200

    ihu

--- server.js
+++ server.js
@@ -1,5 +1,16 @@
-const app = require('./app')
+const app = require('./app');
+const mongoose = require("mongoose");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const { MONGO_DB_URI, PORT = 5000 } = process.env;
+mongoose.set('strictQuery', true);
+
+mongoose.connect(MONGO_DB_URI)
+  .then(() => {
+    app.listen(PORT, () => {
+            console.log(`Server running. Use our API on port: ${PORT}`);
+          });
+  })
+  .catch(error => {
+    console.log(error.message);
+    process.exit(1);
+  })
